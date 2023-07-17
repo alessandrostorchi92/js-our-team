aae804cca6941453be5f58d18c82b98ab0f17e3f
alessandrostorchi92 <storchi.alle@gmail.com>
2023-07-17 22:50:48 +0200

    Milestone 0

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// Consegna:
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
+
+// Procedimento:
+
+// MILESTONE 0: 
+
+// Definire la variabile che conterrà l'array di oggetti
+// Inserire all'interno di questo array i dati forniti(N.B. Inserisci la virgola dopo ogni valore e ricordati di formattare per una corretta visualizzazione dei dati)
+
+const teamList = [
+    {
+        firstName: "Wayne",
+        lastName: "Barnett",
+        role: "Founder & CEO",
+        profilePic: "img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        firstName: "Angela",
+        lastName: "Caroll",
+        role: "Chief Editor",
+        profilePic: "img/angela-caroll-chief-editor.jpg",
+    },
+    {
+        firstName: "Walter",
+        lastName: "Gordon",
+        role: "Office Manage",
+        profilePic: "img/walter-gordon-office-manager.jpg",
+    },
+    {
+        firstName: "Angela",
+        lastName: "Lopez",
+        role: "Social Media Manager",
+        profilePic: "img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+        firstName: "Scott",
+        lastName: "Estrada",
+        role: "Developer",
+        profilePic: "img/scott-estrada-developer.jpg",
+    },
+    {
+        firstName: "Barbara",
+        lastName: "Ramos",
+        role: "Graphic Designer",
+        profilePic: "img/barbara-ramos-graphic-designer.jpg",
+    },
+];
