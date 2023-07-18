1459dd923e2f1afa6db9a4272638198031295ca0
alessandrostorchi92 <storchi.alle@gmail.com>
2023-07-18 09:25:31 +0200

    Inserimento delle immagini

--- js/main.js
+++ js/main.js
@@ -19,37 +19,37 @@ const teamList = [
         firstName: "Wayne",
         lastName: "Barnett",
         role: "Founder & CEO",
-        img: "img/wayne-barnett-founder-ceo.jpg",
+        img: "wayne-barnett-founder-ceo.jpg",
     },
     {
         firstName: "Angela",
         lastName: "Caroll",
         role: "Chief Editor",
-        img: "img/angela-caroll-chief-editor.jpg",
+        img: "angela-caroll-chief-editor.jpg",
     },
     {
         firstName: "Walter",
         lastName: "Gordon",
         role: "Office Manage",
-        img: "img/walter-gordon-office-manager.jpg",
+        img: "walter-gordon-office-manager.jpg",
     },
     {
         firstName: "Angela",
         lastName: "Lopez",
         role: "Social Media Manager",
-        img: "img/angela-lopez-social-media-manager.jpg",
+        img: "angela-lopez-social-media-manager.jpg",
     },
     {
         firstName: "Scott",
         lastName: "Estrada",
         role: "Developer",
-        img: "img/scott-estrada-developer.jpg",
+        img: "scott-estrada-developer.jpg",
     },
     {
         firstName: "Barbara",
         lastName: "Ramos",
         role: "Graphic Designer",
-        img: "img/barbara-ramos-graphic-designer.jpg",
+        img: "barbara-ramos-graphic-designer.jpg",
     },
 ];
 
@@ -67,22 +67,17 @@ for (let key in teamList) {
 
 // MILESTONE 2:
 
-//Creo le cards
-
+//Creo le cards attraverso un ciclio for
 
 for (let i = 0; i <= teamList.length; i++) {
-    
+
     const cards = document.getElementById("cards-container");
-    const memberCard = teamList[i];
+    const teamMember = teamList[i];
 
     //Stampo le cards all'interno del div con id ("cards-container") in html
 
-    cards.innerHTML +=
-    <img src="img/${memberCard.img}" class="card-img-top" alt="Member image">
-    <div class="card-body text-center">
-    <h3 class="card-title">${memberCard.firstName + " " + memberCard.lastName}</h3>
-    <h4 class="card-text">${memberCard.role}</h4>
-    </div>
+    cards.innerHTML += `<img src="img/${teamMember.img}" class="card-img-top" alt="Member image">
+    <div class="card-body text-center"><h3 class="card-title">${teamMember.firstName + " " + teamMember.lastName}</h3><h5 class="card-text">${teamMember.role}</h5></div>`
 
 }
 
